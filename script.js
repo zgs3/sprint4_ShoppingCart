6585ad7879ae49a9ee750510e8ada503db6725cf
zgs3 <zkairaitis@gmail.com>
2022-04-28 15:36:17 +0300

    Added function to delete main list. Updated CSS

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 const listsContainer = document.querySelector('[data-lists]')
 const newListForm = document.querySelector('[data-new-list-form')
 const newListInput = document.querySelector('[data-new-list-input')
+const deleteListButton = document.querySelector('[data-delete-list-button]')
 
+// local storage key's
 const LOCAL_STORAGE_LIST_KEY = 'task.lists'
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId'
+
 // getting list items from LS or an empty array if LS is empty
 let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || []
+// getting selected item ID from LS
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY)
+
+
+deleteListButton.addEventListener('click', e => {
+  lists = lists.filter(list => list.id !== selectedListId)
+  selectedListId = null
+  saveAndRender()
+})
+
+listsContainer.addEventListener('click', e => {
+  if (e.target.tagName.toLowerCase() === 'li') {
+    selectedListId = e.target.dataset.listId
+    saveAndRender()
+  }
+})
 
 newListForm.addEventListener('submit', e => {
   e.preventDefault()
@@ -29,16 +49,20 @@ function saveAndRender() {
 // function for saving list item to LS
 function save() {
   localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists))
+  localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId)
 }
 
 function render() {
-  // creating list item
   clearElement(listsContainer)
   lists.forEach(list => {
     const listElement = document.createElement('li')
     listElement.dataset.listId = list.id // identifying which item is clicked on by id
     listElement.classList.add('list-name')
     listElement.innerText = list.name
+
+    if (list.id === selectedListId) {
+      listElement.classList.add('active-list')
+    }
     listsContainer.append(listElement)
   })
 }
